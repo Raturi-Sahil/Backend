e4c915949240a120688bcf3bad0bb594dd94a61a
Sahil Raturi <sahilraturi687@gmail.com>
2025-06-17 23:39:34 +0530

    signup and signin logic

--- 8_1_backend_course_selling/index.js
+++ 8_1_backend_course_selling/index.js
@@ -4,7 +4,11 @@ const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 
 const { authorize } = require('./auth');
-const { mongo } = require('mongoose');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { UserModel } = require('./models/user');
+const { JWT_SECRET } = require('../7_2_password_auth/auth');
+
 // const z = require('zod');
 
 // what does this line even do ?
@@ -18,33 +22,95 @@ mongoose.connect(DB_URL).then(() => console.log("connected to DB")).catch(err =>
 app.use(express.json());
 
 
-app.post('/user/signup', authorize('user'), function(req, res) {
+app.post('/signup', async function(req, res) {
+    try{
+        const {username, email, password, role} = req.body;
+        if(!username || !email || !password || !role)
+        return res.status(400).json({msg: "All fields are required"});
+
+        const user = await UserModel.findOne({email});
+
+        if(user)
+        return res.status(409).json({msg: "user already exists"});
+
+        //Hashing using bcrypt
+        const hashedPassword = await bcrypt.hash(password, 4);
+
+        const response = await UserModel.create({username, email, password: hashedPassword, role});
+
+        if(!response)
+        return res.status(500).json({msg: "User creation failed, please try again later"});
+
+        res.status(200),json({msg: "You are signed up successfully"});
+
+    } catch(error) {
+        if(error.code === 11000 && error.keyPattern?.email)
+            return res.status(409).json({msg: "Email already in use"});
+
+        return res.status(500).json({msg: "An unexpected error occured during signup"});
+    }
 
 });
-app.post('/user/signin', authorize('user'), function(req, res) {
+app.post('/signin', async function(req, res) {
+    try {
+        const {email, password} = req.body;
 
+        //check if all input fields
+        if(!email || !password)
+            return res.status(400).json({msg: "All fields are required"});
+
+        //fetch the user via just the email
+        const user = await UserModel.findOne({email});
+
+        //if the user doesn't exist then simply return
+        if(!user)
+            return res.status(409).json({msg: "Invalid credentials"});
+
+        //Password authentication
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        //if password auth fails then just return
+        if(!passwordMatch) return res.status(409).json({msg: "Invalid creadentials"});
+
+        //if the user exists we go on to create a token through jsonwebtoken library.
+        const autherization = jwt.sign({id: user._id}, JWT_SECRET);
+
+        //if tokne creation fails then return
+        if(!autherization)
+            return res.status(500).json({msg: "An error occured during sign in, please try again later"});
+        
+        res.status(201).json({msg: "You are successfully signed in", autherization});
+
+
+    }catch(error) {
+        console.error(error);
+        res.status(500).json({msg: "An error occured during sign in, please try again later"});
+    }   
+});
+
+
+app.post('/user/courses', authorize('user'), function(req, res) {
+    // this  endpoint shows all courses available for a user to purchase.
 });
 app.post('/user/purchases', authorize('user'), function(req, res) {
-
+    //this endpoint let's a user make a purchase. 
 });
 app.get('/user/purchases', authorize('user'), function(req, res) {
-
+    //this endpoint shows all the purchases a user has made.
 });
 
-// write the app.use(admin auth middleware here)
 
-
-app.post('/admin/signup', authorize('admin'), function(req, res) {
-
+app.delete('/admin/courses/:courseId', authorize('admin'), function(req, res) {
+    // this endpoint let's an admin delete a course.
 });
-app.post('/admin/signin', authorize('admin'), function(req, res) {
-
+app.post('/admin/courses', authorize('admin'), function(req, res) {
+    // this endpoint let's an admin create a new course. 
 });
-app.delete('/admin/courses', authorize('admin'), function(req, res) {
-
+app.put('/admin/courses/:courseId', authorize('admin'), function(req, res) {
+    // this endpoint let's an admin update a course. 
 });
-app.put('/admin/courses', authorize('admin'), function(req, res) {
-
+app.get('/admin/courses', authorize('admin'), function(req, res) {
+    // this endpoint let's an admin to fetch all the courses they have created. 
 });
 
 app.listen(PORT, ()=> {
--- 8_1_backend_course_selling/models/course.js
+++ 8_1_backend_course_selling/models/course.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema();
-const ObjectId = Schema.ObjectId();
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
 
 const CourseSchema = new Schema({
