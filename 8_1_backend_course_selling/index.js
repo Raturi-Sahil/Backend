3d947946b3acc5e67c9a856300f921a5095e0efe
Sahil Raturi <sahilraturi687@gmail.com>
2025-06-18 20:11:15 +0530

    Implement routes and update schema

--- 8_1_backend_course_selling/index.js
+++ 8_1_backend_course_selling/index.js
@@ -7,6 +7,8 @@ const { authorize } = require('./auth');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const { UserModel } = require('./models/user');
+const { CourseModel } = require('./models/course');
+const { PurchaseModel } = require('./models/purchase');
 const { JWT_SECRET } = require('../7_2_password_auth/auth');
 
 // const z = require('zod');
@@ -41,7 +43,7 @@ app.post('/signup', async function(req, res) {
         if(!response)
         return res.status(500).json({msg: "User creation failed, please try again later"});
 
-        res.status(200),json({msg: "You are signed up successfully"});
+        res.status(201).json({msg: "You are signed up successfully"});
 
     } catch(error) {
         if(error.code === 11000 && error.keyPattern?.email)
@@ -51,6 +53,8 @@ app.post('/signup', async function(req, res) {
     }
 
 });
+
+
 app.post('/signin', async function(req, res) {
     try {
         const {email, password} = req.body;
@@ -64,53 +68,158 @@ app.post('/signin', async function(req, res) {
 
         //if the user doesn't exist then simply return
         if(!user)
-            return res.status(409).json({msg: "Invalid credentials"});
+            return res.status(401).json({msg: "Invalid credentials"});
 
         //Password authentication
         const passwordMatch = await bcrypt.compare(password, user.password);
 
         //if password auth fails then just return
-        if(!passwordMatch) return res.status(409).json({msg: "Invalid creadentials"});
+        if(!passwordMatch) return res.status(401).json({msg: "Invalid credentials"});
 
         //if the user exists we go on to create a token through jsonwebtoken library.
-        const autherization = jwt.sign({id: user._id}, JWT_SECRET);
+        const authorization = jwt.sign({id: user._id}, JWT_SECRET);
 
         //if tokne creation fails then return
-        if(!autherization)
+        if(!authorization)
             return res.status(500).json({msg: "An error occured during sign in, please try again later"});
         
-        res.status(201).json({msg: "You are successfully signed in", autherization});
+        res.status(201).json({msg: "You are successfully signed in", authorization});
 
 
     }catch(error) {
         console.error(error);
-        res.status(500).json({msg: "An error occured during sign in, please try again later"});
+        return res.status(500).json({msg: "An error occured during sign in, please try again later"});
     }   
 });
 
 
-app.post('/user/courses', authorize('user'), function(req, res) {
-    // this  endpoint shows all courses available for a user to purchase.
+app.get('/courses', authorize('user'), async function(req, res) {
+    try {
+        // this endpoint shows all courses available for a user to purchase.
+        const courses = await CourseModel.find();// fetch all the course.
+
+        if(!courses)
+            return res.json({msg: "No course created"}); 
+
+        res.status(200).json(courses);// send to the frontend.
+    }catch(error) {
+        console.error(error);
+        res.status(500).json({msg: "An error occured during fetching the courses"})
+    }
 });
-app.post('/user/purchases', authorize('user'), function(req, res) {
-    //this endpoint let's a user make a purchase. 
+
+app.post('/user/purchases', authorize('user'), async function(req, res) {
+    try {
+        //this endpoint let's a user make a purchase. 
+        const userId = req.userId;
+        const courseId = req.body.courseId;
+
+        if(!courseId)
+            return res.status(400).json({msg: "Invalid input"});
+
+        const existingPurchase = await PurchaseModel.findOne({userId, courseId});
+        if(existingPurchase)
+            return res.status(409).json({msg: "Course already purchased"});
+
+        const purchase = await PurchaseModel.create({userId, courseId});
+
+        if(!purchase)
+            return res.status(500).json({msg: "An error occured during purchase"});
+
+        res.status(201).json({msg: "Transaction successful", purchase});
+    } catch(error) {
+        console.error(error);
+        return res.status(500).json({msg: "Internal server error"});
+    }
 });
-app.get('/user/purchases', authorize('user'), function(req, res) {
-    //this endpoint shows all the purchases a user has made.
+app.get('/user/purchases', authorize('user'),async function(req, res) {
+    try {
+        //this endpoint let's a user make a purchase. 
+        const userId = req.userId;
+        const purchases = await PurchaseModel.findOne({userId});
+        res.status(200).json(purchases);
+    } catch(error) {
+        res.status(500).json({msg: "Internal server error"});
+    }
 });
 
 
-app.delete('/admin/courses/:courseId', authorize('admin'), function(req, res) {
+app.delete('/admin/courses/:id', authorize('admin'), async function(req, res) {
     // this endpoint let's an admin delete a course.
+    try {
+        const courseId = req.params.id;
+        // no need to check for course id, if it hadn't been there then the requiest wouldn't have hit this endpoint.
+
+        const response = await CourseModel.findByIdAndDelete(courseId);
+        
+        if(!response)
+            res.status(500).json({msg: "Internal server error"});
+
+        res.status(204).json({msg: "Course successfully deleted", response});
+    }catch(error) {
+        console.error(error);
+        return res.status(500).json({msg: "Internal server error"});
+    }
+
 });
-app.post('/admin/courses', authorize('admin'), function(req, res) {
-    // this endpoint let's an admin create a new course. 
+app.post('/admin/courses', authorize('admin'), async function(req, res) {
+    try {
+            // this endpoint let's an admin create a new course. 
+        const userId = req.userId;
+        const { title, description, price } = req.body;
+
+        if(!title || !description || !price)
+            return res.status(400).json({msg: "Please fill all the details"});
+
+        const course = await CourseModel.create({title, description, price, userId});
+
+        // if course creation fails
+        if(!course)
+            return res.status(500).json({msg: "Internal server error"});
+
+        res.status(201).json({msg: "Course successfully created"}, course);
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({msg: "Internal server error"});
+    }
 });
-app.put('/admin/courses/:courseId', authorize('admin'), function(req, res) {
-    // this endpoint let's an admin update a course. 
+app.put('/admin/courses/:id', authorize('admin'), async function(req, res) {
+    try {
+        // this endpoint let's an admin update a course. 
+        const courseId = req.params.id;
+        const updates = req.body;
+
+        // no need to check for course id, if it hadn't been there then the requiest wouldn't have hit this endpoint.
+
+        if(!updates)
+            return res.status(400).json({msg: "Nothing to update"});
+
+        const updatedCourse = await CourseModel.findByIdAndUpdate(courseId, {$set: updates}, {new: true, runValidators: true});
+
+        if(!updatedCourse)
+            return res.status(500).json({msg: "Error during update"});
+
+        res.status(200).json({msg: "Course updated", updatedCourse});
+
+    }catch(error) {
+        console.error(error);
+        res.status(500).json({msg: "Internal server error"});
+    }
 });
-app.get('/admin/courses', authorize('admin'), function(req, res) {
-    // this endpoint let's an admin to fetch all the courses they have created. 
+app.get('/admin/courses', authorize('admin'), async function(req, res) {
+    
+    try {
+        // this endpoint let's an admin to fetch all the courses they have created. 
+        const userId = req.userId;
+        const courses = await CourseModel.find({userId});// fetch all the course. 
+        if(!courses)
+            return res.json({msg: "No course created", userId});
+
+        res.status(200).json(courses);// send to the frontend.
+    }catch(error) {
+        console.error(error);
+        res.status(500).json({msg: "An error occured during fetching the courses"})
+    }
 });
 
 app.listen(PORT, ()=> {
--- 8_1_backend_course_selling/models/purchase.js
+++ 8_1_backend_course_selling/models/purchase.js
@@ -7,7 +7,10 @@ const PurchaseSchema = new Schema({
     userId: {type: ObjectId, ref: 'User', required: true},
     courseId: {type: ObjectId, ref: 'Course', required: true},
     purchaseDate: {type: Date, default: Date.now},
-    validTill: {type: Date}
+    validTill: {type: Date, default: function() {
+        const oneYearFromNow = new Date();
+        oneYearFromNow.setFullYear(oneYearFromNow.getFullYear()+1);
+    }}
 });
 
 
